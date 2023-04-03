ab92409c134d4bb78acfd7758c13a893c61836ee
Jon Schulman <25127543+vaficionado@users.noreply.github.com>
2023-04-03 10:10:31 -0700

    updating readme and user key

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import requests
 app = Flask(__name__)
 
 user = {
-    "key": "aa0ceb",
+    "key": "aa0ceb1234",
     "firstName": "Mara",
     "lastName": "Jade",
     "email": "mjade@coruscant.gov",
